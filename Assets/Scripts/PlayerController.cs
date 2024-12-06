8c02e36243a084a66c3e75ab44cd85ef0b0b5a00
Untitled26 <cr3at0r0fw0r1d5@gmail.com>
2024-12-06 17:41:38 -0500

    commit v01.004 - (20241206)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,74 +8,72 @@ using static UnityEditor.VersionControl.Asset;
 public class PlayerController : MonoBehaviour
 {
     // fuck with people or stop murders before your own time runs out.
+    /// TO DO LIST
+    /// 1. FIX JUMP
+    /// 2. 
+    /// 
+    /// 
+    /// 
+    /// 
+    /// TO DO LIST
 
 
+
+    // VARS
     //movement and control Logic
     private Vector2 mouse;
-    public GameObject cursor;
-    private Rigidbody2D rb;
-    private float mx;
-    private float my;
-    private bool postjumpress;
-    private bool jump; 
+        public GameObject cursor;
+        private Rigidbody2D rb;
+        private float mx;
+        private float my;
+        private bool postjumpress;
+        //private bool jump; 
 
+        // PLAYER STATS
+        public float PlayerLife = 3f;
+        public float PlayerSpeed = 10f;
+        public float JumpHeight = 10f;
 
-    // PLAYER STATS
-    public float PlayerLife = 3f;
-    public float PlayerSpeed = 10f;
-    public float JumpHeight = 10f;
+        // States
+        public enum Playerstates
+        {
+            LIVING,
+            LIMINAL,
+            GHOST,
+            DEAD
+        }
+        public Playerstates Pstate;
+        public Playerstates prevstate;
+        //private Playerstates prevstate;
 
-    // States
-    public enum Playerstates
-    {
-        LIVING,
-        LIMINAL,
-        GHOST,
-        DEAD
-    }
-    public Playerstates Pstate;
-    public Playerstates prevstate;
-    //private Playerstates prevstate;
-
-
-
-
-
-
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        Pstate = Playerstates.LIVING;
-        UnityEngine.Cursor.visible = false;
-        postjumpress = false;
-        jump = false;
-}
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    // DECLARES
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Pstate = Playerstates.LIVING;
+            UnityEngine.Cursor.visible = false;
+            postjumpress = true;
+            //jump = false;
     }
 
+  
 
 
-
-
-    // Update is called once per frame
+    // MOST INPUTS:
+    // BY FRAMERATE TICK REFRESH
     void Update()
     {
         //Debug.Log(postjumpress);
         //Debug.Log(Pstate.ToString());
 
         // place different control settings in each case!!!
-        mx = Input.GetAxisRaw("Horizontal");
 
 
         // MOUSE /////////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log(mouse);
         cursor.transform.position = mouse;
+        //Debug.Log(mouse);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -86,10 +84,9 @@ public class PlayerController : MonoBehaviour
         //////////////////////////////////////////////////////////////////
         if (Input.GetKeyDown("q"))
         {   // TRANSFORM!!
-            prevstate = Pstate;
             if (Pstate == Playerstates.LIVING || Pstate == Playerstates.LIMINAL)
             {
-                Debug.Log("shitfuck");
+                prevstate = Pstate;
                 Pstate = Playerstates.GHOST;
             } else if (Pstate == Playerstates.GHOST)
             {
@@ -98,6 +95,16 @@ public class PlayerController : MonoBehaviour
             Debug.Log(Pstate.ToString());
         }
 
+
+
+
+    }
+
+    // BY PHYSICS TICK REFRESH
+    private void FixedUpdate()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(mx, my).normalized * PlayerSpeed;
 
 
         // SWITCH ////////////////////////////////////////////////////////
@@ -113,7 +120,7 @@ public class PlayerController : MonoBehaviour
                     {
                         //Debug.Log("trying");
                         postjumpress = true;
-                        jump = true;
+                        rb.AddForce(transform.up * JumpHeight, ForceMode2D.Impulse);
                     }
                 }
                 if (Input.GetKeyDown("left shift"))
@@ -143,25 +150,17 @@ public class PlayerController : MonoBehaviour
     }
 
 
-
-
-
-    private void FixedUpdate()
-    {
-
-        rb.velocity = new Vector2(mx, my).normalized * PlayerSpeed;
-        if(jump == true)
-        {
-            jump = false;
-            rb.AddForce(transform.up * JumpHeight, ForceMode2D.Impulse);
-        }
-    }
-
+    // FUNCTIONS
     private void ActiveHeldObj()
     {
 
     }
 
+
+
+
+
+    // COLLISION HANDLING
     public void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Player has collided with: " + collision.gameObject.name);
